4ffaa005dc07baebea44c15ad88dd577869a3ea7
2078440P <2078440P@student.gla.ac.uk>
2015-03-01 23:02:31 +0000

    (Chapter 12 completed)

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rango',
     'polls',
+    'registration'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -72,7 +73,11 @@ PASSWORD_HASHERS = (
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-LOGIN_URL = '/rango/login/'
+REGISTRATION_OPEN = True                # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
 
 
 DATABASES = {
